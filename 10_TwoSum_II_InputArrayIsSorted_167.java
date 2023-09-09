7675624a5209c1cc4cb9726ec8955b0d52d017d7
Nikhil Jadhav <102472043+jadhavnikhil2624@users.noreply.github.com>
2023-09-09 19:50:12 +0530

    Tenth Commit

--- 10_TwoSum_II_InputArrayIsSorted_167.java
+++ 10_TwoSum_II_InputArrayIsSorted_167.java
@@ -1,17 +1,31 @@
-class Solution {
+class TwoSum_II_InputArrayIsSorted_167 {
 
-  public int[] twoSum(int[] numbers, int target) {
+  public static int twoSum(int[] numbers, int target) {
     int start = 0;
     int end = numbers.length - 1;
+    int arr1[] = new int[2];
     while (start < end) {
       if (numbers[start] + numbers[end] > target) {
         end--;
       } else if (numbers[start] + numbers[end] < target) {
         start++;
       } else {
-        return new int[] { start + 1, end + 1 }; // +1 because our output index start from 1.
+        // +1 because our output index start from 1.
+        arr1[0] = start;
+        arr1[1] = end;
       }
     } //End while
-    return new int[] {}; //Return empty array
+    return 0; //Return empty array
   } //End twoSum
+
+  public static void main(String[] args) {
+    int arr[] = new int[10];
+    arr[0] = 4;
+    arr[1] = 5;
+    arr[2] = 4;
+    arr[3] = 5;
+    int target = 9;
+
+    System.out.println(twoSum(arr, target));
+  } //End main
 } //End Solution
